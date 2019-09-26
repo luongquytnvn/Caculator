a2814f4e2f945775b6e91c9e47be8c19bfb4bf04
luongquytnvn <luongquytnvn@gmail.com>
2019-09-26 22:21:53 +0700

    bai mo rong

---
+++ Caculator2.js
@@ -0,0 +1,60 @@
+function a1() {
+    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 1
+}
+function a2() {
+    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 2
+}
+function a3() {
+    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 3
+}
+function a4() {
+    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 4
+}
+function a5() {
+    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 5
+}
+function a6() {
+    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 6
+}
+function a7() {
+    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 7
+}
+function a8() {
+    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 8
+}
+function a9() {
+    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 9
+}
+function a0() {
+    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 0
+}
+function a02() {
+    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ '-'
+}
+function a03() {
+    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 'x'
+}
+function a04() {
+    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ ':'
+}
+function a01() {
+    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ "+"
+}
+function a05() {
+    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ "."
+}
+function a06() {
+    document.getElementById("idmanhinh").value = eval(document.getElementById("idmanhinh").value)
+}
+function a07() {
+    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+'('
+}
+function a08() {
+    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+')'
+}
+function a09() {
+    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+'%'
+}
+function a10() {
+    document.getElementById("idmanhinh").value = ''
+}
