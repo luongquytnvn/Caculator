c54332adc19055cbad238177cb599ac99e3534ad
luongquytnvn <luongquytnvn@gmail.com>
2019-10-02 21:08:56 +0700

    cập nhật tính năng

--- Caculator2.js
+++ Caculator2.js
@@ -1,60 +1,50 @@
-function a1() {
-    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 1
+let a = false;
+let b= false;
+let c = false;
+
+function number(tinh1) {
+    if (a&&b&&c) {
+        document.getElementById("idmanhinh").value = '';
+        document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ tinh1.value;
+    } else {
+        document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value + tinh1.value;
+    }
+    a=true;
+    b=false;
+    c=true;
 }
-function a2() {
-    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 2
+function pheptinh(tinh) {
+    if (a&&b&&c || a&&c&&(b==false)|| c&&b&&(a==false)) {
+        document.getElementById("idmanhinh").value += tinh.value;
+    }
+    a=true;
+    b=true;
+    c=false;
 }
-function a3() {
-    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 3
-}
-function a4() {
-    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 4
-}
-function a5() {
-    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 5
-}
-function a6() {
-    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 6
-}
-function a7() {
-    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 7
-}
-function a8() {
-    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 8
-}
-function a9() {
-    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 9
-}
-function a0() {
-    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ 0
-}
-function a02() {
-    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ '-'
-}
-function a03() {
-    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ '*'
-}
-function a04() {
-    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ ':'
-}
-function a01() {
-    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ "+"
-}
-function a05() {
-    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ "."
+function kyhieu(kyhieu1) {
+    if (a&&b&&c) {
+        document.getElementById("idmanhinh").value = '';
+        document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+ kyhieu1.value;
+    } else {
+        document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value + kyhieu1.value;
+    }
 }
 function a06() {
-    document.getElementById("idmanhinh").value = eval(document.getElementById("idmanhinh").value)
-}
-function a07() {
-    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+'('
-}
-function a08() {
-    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+')'
-}
-function a09() {
-    document.getElementById("idmanhinh").value = document.getElementById("idmanhinh").value+'%'
+    if (a&&c&&(b==false)||c&&b&&(a==false)) {
+        document.getElementById("idmanhinh").value = eval(document.getElementById("idmanhinh").value);
+    }
+    a=true;
+    b=true;
+    c=true;
 }
 function a10() {
-    document.getElementById("idmanhinh").value = ''
+    document.getElementById("idmanhinh").value = '';
+    a = false;
+    b= false;
+    c = false;
+}
+function del() {
+    i = document.getElementById("idmanhinh").value;
+    j = i.length;
+    document.getElementById("idmanhinh").value = i.substr(0,j-1);
 }
